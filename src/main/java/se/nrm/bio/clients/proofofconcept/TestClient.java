d58be68dbfeba667204e7c201b9d386db6cddc4f
Ingimar Erlingsson <inkimar@gmail.com>
2019-11-13 20:37:53 +0100

    removing unused methods and imports

--- src/main/java/se/nrm/bio/clients/proofofconcept/TestClient.java
+++ src/main/java/se/nrm/bio/clients/proofofconcept/TestClient.java
@@ -1,16 +1,8 @@
 package se.nrm.bio.clients.proofofconcept;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -21,9 +13,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import org.apache.log4j.Logger;
-import org.glassfish.jersey.media.multipart.FormDataBodyPart;
-import org.glassfish.jersey.media.multipart.FormDataMultiPart;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 /**
  * java -Xms512m -Xmx15G
@@ -43,14 +32,14 @@ public class TestClient {
 
         if (healthOfServer == 200) {
             System.out.println("Server is running \n");
-//            c.other();
-           c.testing();
+            HttpResponse response = c.testing();
+            int statusCode = response.getStatusLine().getStatusCode();
+            System.out.println("Client posted and response was "+statusCode);
         } else {
             System.out.println("Server is not running ");
         }
     }
 
-    // 2019-11-13, fungerar, har alltför många dependencies i pom.xml -kolla igenom
     private int healthOfServer() throws IOException {
         int status = 404;
         CloseableHttpClient httpclient = HttpClients.createDefault();
@@ -61,67 +50,7 @@ public class TestClient {
 
     }
 
-    /**
-     * the uploaded file does not have the same md5sum !?
-     *
-     * @throws FileNotFoundException
-     * @throws IOException
-     */
-    private void png() throws FileNotFoundException, IOException {
-        final Client client = ClientBuilder.newBuilder().register(MultiPartFeature.class).build();
-
-        String fileName = "testbild-svt-666.png";
-        String filePath = "/tmp/".concat(fileName);
-        System.out.println("File ".concat(filePath));
-
-        int i = 0;
-
-        FormDataMultiPart form = new FormDataMultiPart();
-        form.field("owner", "ingimar");
-        form.field("fileName", fileName);
-        form.field("workgroupId", "XXX");
-        form.field("userId", Integer.toString(i));
-        InputStream content = new FileInputStream(new File(filePath));
-        FormDataBodyPart fdp = new FormDataBodyPart("content", content, MediaType.APPLICATION_OCTET_STREAM_TYPE);
-        form.bodyPart(fdp);
-
-        final WebTarget target = client.target("http://localhost:8080/JerseyServer/rest/upload/png");
-        final Response response = target.request().post(Entity.entity(form, MediaType.MULTIPART_FORM_DATA));
-        content.close();
-        System.out.println("response " + response.getStatus());
-    }
-
-    private void other() throws FileNotFoundException, IOException {
-        final Client client = ClientBuilder.newBuilder().register(MultiPartFeature.class).build();
-        System.out.println("Testclient OTHER");
-
-        String fileName = "testbild-svt-666.png";
-//        String fileName = "500mb.zip";
-//        String fileName = "1000mb.zip";
-//        String fileName = "2GB.zip";
-        String filePath = "/tmp/".concat(fileName);
-
-        System.out.println("File ".concat(filePath));
-
-        int i = 0;
-
-        FormDataMultiPart form = new FormDataMultiPart();
-        form.field("owner", "ingimar");
-        form.field("fileName", fileName);
-        form.field("workgroupId", "XXX");
-        form.field("userId", Integer.toString(i));
-        InputStream content = new FileInputStream(new File(filePath));
-        FormDataBodyPart fdp = new FormDataBodyPart("content", content, MediaType.APPLICATION_OCTET_STREAM_TYPE);
-        form.bodyPart(fdp);
-
-        final WebTarget target = client.target("http://localhost:8080/JerseyServer/rest/upload/other");
-        final Response response = target.request().post(Entity.entity(form, MediaType.MULTIPART_FORM_DATA));
-        content.close();
-        System.out.println("response " + response.getStatus());
-        System.out.println("response " + response.getStatusInfo());
-    }
-
-    private void testing() throws FileNotFoundException, IOException {
+    private HttpResponse testing() throws FileNotFoundException, IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         String fileName = "testbild-svt-666.png";
         String filePath = "/tmp/".concat(fileName);
@@ -131,6 +60,8 @@ public class TestClient {
         HttpPost httpPost = new HttpPost("http://localhost:8080/JerseyServer/rest/upload/other");
         httpPost.setEntity(entity);
         HttpResponse response = httpClient.execute(httpPost);
-        HttpEntity result = response.getEntity();
+
+//        HttpEntity result = response.getEntity();
+        return response;
     }
 }
